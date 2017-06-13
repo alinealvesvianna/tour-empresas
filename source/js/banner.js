6d108a0db861cc732b244c1e795c6dc4ab9b03f1
Aline Vianna <aline@alinevianna.com.br>
2017-06-13 00:09:50 -0300

    feat: criando métodos para manipular dados

--- source/js/banner.js
+++ source/js/banner.js
@@ -1,36 +1,51 @@
-(function () {
+(function() {
 
-    var ConfiguracoesBanner = {
-        opcoesBoxes: [
-            {
-                tipoTransacao: "abertura",
-                titulo: "Abrir",
-                texto: "Veja aqui como <strong class='u-maiuscula'>abrir</strong> a sua empresa",
-                textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
-            },
-            {
-                tipoTransacao: "alteracao",
-                titulo: "Alterar",
-                texto: "Veja aqui como <strong class='u-maiuscula'>alterar</strong> a sua empresa",
-                textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
-            },
-            {
-                tipoTransacao: "baixa",
-                titulo: "Dar Baixa",
-                texto: "Veja aqui como <strong class='u-maiuscula'>dar baixa</strong> na sua empresa",
-                textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
-            }
+  var ConfiguracoesBanner = {
+    opcoesBoxes: [{
+        tipoTransacao: "abertura",
+        titulo: "Abrir",
+        texto: "Veja aqui como <strong class='u-maiuscula'>abrir</strong> a sua empresa",
+        textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
+      },
+      {
+        tipoTransacao: "alteracao",
+        titulo: "Alterar",
+        texto: "Veja aqui como <strong class='u-maiuscula'>alterar</strong> a sua empresa",
+        textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
+      },
+      {
+        tipoTransacao: "baixa",
+        titulo: "Dar Baixa",
+        texto: "Veja aqui como <strong class='u-maiuscula'>dar baixa</strong> na sua empresa",
+        textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
+      }
 
-        ],
+    ],
 
-        opcoesEscolhaTipoEmpresa: {
-            titulo: "",
-            bannerDescricao: "Primeiro, selecione o <stong class='u-maiuscula u-bold'>tipo da empresa:</stong>",
-            tiposEmpresas: ["Matriz", "Filial", "MEI"],
-            textoBnt: "continuar"
-        }
+    opcoesEscolhaTipoEmpresa: {
+      titulo: "",
+      bannerDescricao: "Primeiro, selecione o <stong class='u-maiuscula u-bold'>tipo da empresa:</stong>",
+      tiposEmpresas: ["Matriz", "Filial", "MEI"],
+      textoBnt: "continuar"
+    }
 
-    };
+  };
 
+
+  var ManipularConfiguracoes = {
+    init: function() {
+      BannerPrincipal.init();
+    },
+
+    dadosConfiguracaoBanner: function() {
+      return ConfiguracoesBanner.opcoesBoxes;
+    },
+
+    dadosConfiguracoesTipoEmpresa: function() {
+      return ConfiguracoesBanner.opcoesEscolhaTipoEmpresa;
+    }
+  };
+
+  ManipularConfiguracoes.init();
 
 })();
