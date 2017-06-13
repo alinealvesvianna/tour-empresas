77c4aad262332294c18bf106978fbe403c049af9
Aline Vianna <aline@alinevianna.com.br>
2017-06-12 23:48:45 -0300

    feat: Adicionando classe de dados

---
+++ source/js/banner.js
@@ -0,0 +1,36 @@
+(function () {
+
+    var ConfiguracoesBanner = {
+        opcoesBoxes: [
+            {
+                tipoTransacao: "abertura",
+                titulo: "Abrir",
+                texto: "Veja aqui como <strong class='u-maiuscula'>abrir</strong> a sua empresa",
+                textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
+            },
+            {
+                tipoTransacao: "alteracao",
+                titulo: "Alterar",
+                texto: "Veja aqui como <strong class='u-maiuscula'>alterar</strong> a sua empresa",
+                textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
+            },
+            {
+                tipoTransacao: "baixa",
+                titulo: "Dar Baixa",
+                texto: "Veja aqui como <strong class='u-maiuscula'>dar baixa</strong> na sua empresa",
+                textoBtn: "Clique <br /> <span class='u-corAmarelo'>aqui</span>"
+            }
+
+        ],
+
+        opcoesEscolhaTipoEmpresa: {
+            titulo: "",
+            bannerDescricao: "Primeiro, selecione o <stong class='u-maiuscula u-bold'>tipo da empresa:</stong>",
+            tiposEmpresas: ["Matriz", "Filial", "MEI"],
+            textoBnt: "continuar"
+        }
+
+    };
+
+
+})();
